b597c6f74a422a68073f9a3f77d92bd5ce75a56d
Cristian Fabricio Rodriguez Romero <fab7696650@gmail.com>
2021-04-29 23:36:39 -0500

    fist commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Voice number
+
+This is a game to play in the terminal. The idea of this is to practice the sounds of numbers in english to reinforce listening in this language.
+
+## Requeriments
+
+* Python3
+* Pip3
+* Virtualenv(optional)
+* The library mpg321
+
+## How to install?
+
+1. Download or clone this project
+2. Run the command
+```
+$ pip3 install -r requeriments.txt
+```
+
+## How to play?
+
+You only needs to run the command
+
+```bash
+$ python3 number.py
+```
+And follow the instructions
+
+## Sources
+
+I read this sources to create this game
+
+* [Convert text to speech in python](https://www.geeksforgeeks.org/convert-text-speech-python/)
+* [Playing mp3 song on python](https://stackoverflow.com/questions/20021457/playing-mp3-song-on-pytho://stackoverflow.com/questions/20021457/playing-mp3-song-on-python)
+
---
+++ number.py
@@ -0,0 +1,56 @@
+from gtts import gTTS
+from pygame import mixer
+
+import os
+import random
+import sys
+
+print(" ------------------------------")
+print("| Welcome to guess the number  |")
+print(" ------------------------------")
+print("| The idea is that you will    |")
+print("| listen a number and you      |")
+print("| write it, if you guess you   |")
+print("| can continue with the next.  |")
+print("| You have 3 lives, if you lost|")
+print("| these you lose. Good luck    |")
+print(" ------------------------------")
+print("| Now please selected you level|")
+print("| 1 - Easy                     |")
+print("| 2 - Middle                   |")
+print("| 3 - Advance                  |")
+print(" ------------------------------")
+level = input()
+print(" Press Enter to start")
+input()
+
+if (level=='1'):
+    lives = 5
+elif(level=='2'):
+    lives = 3
+elif(level=='3'):
+    lives = 1
+else:
+    print("Upss, you have entered an invalid value")
+    sys.exit()
+
+while(lives > 0):
+
+    number = str(random.randint(0,10000))
+
+    language = 'en'
+
+    audio = gTTS(text=number, lang=language, slow=False)
+    audio.save("number.mp3")
+
+    mixer.init()
+    mixer.music.load('./number.mp3')
+    mixer.music.play()
+    os.remove("number.mp3")
+    i = input("what number did you listen?")
+    if (i == number):
+        print("Congratulations that was the number")
+    else:
+        print("Sorry :( It's wrong")
+        lives = lives - 1
+        print("Now you have " + str(lives) + " lives")
